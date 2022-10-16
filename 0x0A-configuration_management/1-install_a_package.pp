0af2a613f5c15959b61a9783cbcedbaffc0d14a1
Semachew Fasika <eth.saf.1992@gmail.com>
2022-10-16 13:58:49 +0300

    1-install_a_package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# install puppet-lint -v 2.5.0
-
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+# Install puppet-lint
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
